d1f7e1eaf55543ba7afa24dcdeaa3b20fd89d558
alvin3yang <38968358+alvin3yang@users.noreply.github.com>
2018-09-21 22:21:16 +1000

    Add files via upload

---
+++ crc/crc/store.py
@@ -0,0 +1,15 @@
+class Store:
+    store_id = 0
+    store_name = ''
+    store_address = ''
+    store_phone = ''
+    store_city = ''
+    store_state = ''
+
+    def __init__(self):
+        self.store_id = 0
+        self.store_name = ''
+        self.store_address = ''
+        self.store_phone = ''
+        self.store_city = ''
+        self.store_state = ''
