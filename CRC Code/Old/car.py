0c8c9a398410cd9a2dbec396660873fe86b16191
harry16686 <39335304+harry16686@users.noreply.github.com>
2018-10-25 20:05:44 +1000

    Final Code

---
+++ CRC Code/Old/car.py
@@ -0,0 +1,35 @@
+class Car:
+    car_id = 0
+    car_makename = ''
+    car_model = ''
+    car_series = ''
+    car_series_year = ''
+    car_price_new = ''
+    car_engine_size = ''
+    car_fuel_system = ''
+    car_tank_capacity = ''
+    car_power = ''
+    car_seating_capacity = ''
+    car_standard_transmission = ''
+    car_body_type = ''
+    car_drive = ''
+    car_wheelbase = ''
+    store_id = 0
+
+    def __init__(self):
+        self.car_id = 0
+        self.car_makename = ''
+        self.car_model = ''
+        self.car_series = ''
+        self.car_series_year = ''
+        self.car_price_new = ''
+        self.car_engine_size = ''
+        self.car_fuel_system = ''
+        self.car_tank_capacity = ''
+        self.car_power = ''
+        self.car_seating_capacity = ''
+        self.car_standard_transmission = ''
+        self.car_body_type = ''
+        self.car_drive = ''
+        self.car_wheelbase = ''
+        self.store_id = 0
---
+++ CRC Code/Old/customer.py
@@ -0,0 +1,17 @@
+class Customer:
+    customer_id = 0
+    customer_name = ''
+    customer_phone = ''
+    customer_address = ''
+    customer_birthday = ''
+    customer_occupation = ''
+    customer_gender = ''
+
+    def __init__(self):
+        self.customer_id = 0
+        self.customer_name = ''
+        self.customer_phone = ''
+        self.customer_address = ''
+        self.customer_birthday = ''
+        self.customer_occupation = ''
+        self.customer_gender = ''
